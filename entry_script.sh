1e8464afe8b5adf85eb613afc7f27c3bc672a717
george nguafac <princengu98@yahoo.com>
2021-09-23 01:37:31 -0400

    made changes to the entry script

--- entry_script.sh
+++ entry_script.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 sudo yum update -y && sudo yum install -y docker
 sudo systemctl start docker
+sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 docker run -p 8080:80 nginx
                 
